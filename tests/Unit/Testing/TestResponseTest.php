b208b36407dfc0ce535e0b852db3fb79c8847425
Barbosa <omar.barbosa@evertecinc.com>
2023-10-05 19:00:27 -0500

    tests(fix): run server before call assertions

--- tests/Unit/Testing/TestResponseTest.php
+++ tests/Unit/Testing/TestResponseTest.php
@@ -2,17 +2,31 @@
 
 declare(strict_types=1);
 
+use Amp\Http\Server\Response;
+use Phenix\Facades\Route;
+use Phenix\Http\Middlewares\AcceptJsonResponses;
+
+beforeEach(function () {
+    Route::get('/', fn () => new Response(body: 'Hello'))
+        ->middleware(AcceptJsonResponses::class);
+
+    $this->app->run();
+});
+
+afterEach(function () {
+    $this->app->stop();
+});
 
 it('can assert responses', function () {
     $this->get('/')
         ->assertOk()
         ->assertBodyContains('Hello');
 
-    $this->get('/users')
-        ->assertNotFound();
-
     $this->get(path: '/', headers: ['Accept' => 'text/html'])
         ->assertNotAcceptable();
+
+    $this->get('/users')
+        ->assertNotFound();
 
     $this->post('/users', ['name' => 'John Doe'])
         ->assertNotFound();
