1cedaa999de66d7210345ecf532d0db5ea04efe4
Barbosa <omar.barbosa@evertecinc.com>
2023-10-10 17:58:42 -0500

    feat: make service provider command

---
+++ tests/Unit/Console/MakeServiceProviderCommandTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Phenix\Contracts\Filesystem\File;
+use Phenix\Testing\Mock;
+
+it('creates service provider successfully', function () {
+    // $mock = Mock::of(File::class)->expect(
+    //     exists: fn (string $path) => false,
+    //     get: fn (string $path) => '',
+    //     put: function (string $path) {
+    //         expect($path)->toBe(base_path('app/Providers/SlackServiceProvider.php'));
+
+    //         return true;
+    //     },
+    //     createDirectory: function (string $path): void {
+    //         // ..
+    //     }
+    // );
+
+    // $this->app->swap(File::class, $mock);
+
+    /** @var \Symfony\Component\Console\Tester\CommandTester $command */
+    $command = $this->phenix('make:provider', [
+        'name' => 'SlackServiceProvider',
+    ]);
+
+    $command->assertCommandIsSuccessful();
+
+    expect($command->getDisplay())->toContain('Service provider successfully generated!');
+});
