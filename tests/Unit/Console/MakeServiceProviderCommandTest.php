b196dc42cbca648184ff04536fecb1fea573f50c
Barbosa <omar.barbosa@evertecinc.com>
2023-10-10 18:14:13 -0500

    chore: uncomment test code

--- tests/Unit/Console/MakeServiceProviderCommandTest.php
+++ tests/Unit/Console/MakeServiceProviderCommandTest.php
@@ -6,20 +6,20 @@ use Phenix\Contracts\Filesystem\File;
 use Phenix\Testing\Mock;
 
 it('creates service provider successfully', function () {
-    // $mock = Mock::of(File::class)->expect(
-    //     exists: fn (string $path) => false,
-    //     get: fn (string $path) => '',
-    //     put: function (string $path) {
-    //         expect($path)->toBe(base_path('app/Providers/SlackServiceProvider.php'));
+    $mock = Mock::of(File::class)->expect(
+        exists: fn (string $path) => false,
+        get: fn (string $path) => '',
+        put: function (string $path) {
+            expect($path)->toBe(base_path('app/Providers/SlackServiceProvider.php'));
 
-    //         return true;
-    //     },
-    //     createDirectory: function (string $path): void {
-    //         // ..
-    //     }
-    // );
+            return true;
+        },
+        createDirectory: function (string $path): void {
+            // ..
+        }
+    );
 
-    // $this->app->swap(File::class, $mock);
+    $this->app->swap(File::class, $mock);
 
     /** @var \Symfony\Component\Console\Tester\CommandTester $command */
     $command = $this->phenix('make:provider', [
