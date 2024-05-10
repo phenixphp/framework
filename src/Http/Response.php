dfa2c6d845554629d4dcde1ae19c8386c3fa7acb
Omar Barbosa <omar.barbosa@evertecinc.com>
2024-05-10 17:54:20 -0500

    feat: allow pass headers

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -10,22 +10,33 @@ use Phenix\Contracts\Arrayable;
 
 class Response
 {
-    public function plain(string $content, int $status = HttpStatus::OK): ServerResponse
+    public function plain(string $content, int $status = HttpStatus::OK, array $headers = []): ServerResponse
     {
-        return new ServerResponse($status, ['content-type' => 'text/plain'], $content);
+        return new ServerResponse(
+            $status,
+            [...['content-type' => 'text/plain'], ...$headers],
+            $content
+        );
     }
 
     /**
      * @param array<string|int, array|string|int|bool> $content
      */
-    public function json(Arrayable|array $content = [], int $status = HttpStatus::OK): ServerResponse
-    {
+    public function json(
+        Arrayable|array $content = [],
+        int $status = HttpStatus::OK,
+        array $headers = []
+    ): ServerResponse {
         if ($content instanceof Arrayable) {
             $content = $content->toArray();
         }
 
         $body = json_encode(['data' => $content]);
 
-        return new ServerResponse($status, ['content-type' => 'application/json'], $body . PHP_EOL);
+        return new ServerResponse(
+            $status,
+            [...['content-type' => 'application/json'], ...$headers],
+            $body . PHP_EOL
+        );
     }
 }
