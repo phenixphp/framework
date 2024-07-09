2473df661b3f7ae9566c0ca98aa22ba3ed65af1b
Omar Barbosa <omar.barbosa@evertecinc.com>
2024-07-09 15:36:50 -0500

    refactor: resolve closure param in route building

--- src/Http/Requests/ClosureRequestHandler.php
+++ src/Http/Requests/ClosureRequestHandler.php
@@ -10,21 +10,18 @@ use Amp\Http\Server\RequestHandler;
 use Amp\Http\Server\Response as ServerResponse;
 use Closure;
 use Phenix\Http\FormRequest;
-use Phenix\Http\Request;
-use ReflectionFunction;
-use ReflectionNamedType;
-use ReflectionParameter;
 
 class ClosureRequestHandler implements RequestHandler
 {
     public function __construct(
-        private readonly Closure $closure
+        private readonly Closure $closure,
+        private readonly string $formRequest
     ) {
     }
 
     public function handleRequest(ServerRequest $request): ServerResponse
     {
-        $formRequest = $this->resolveFormRequest($request);
+        $formRequest = new $this->formRequest($request);
 
         if ($formRequest instanceof FormRequest) {
             if (! $formRequest->isValid()) {
@@ -37,27 +34,5 @@ class ClosureRequestHandler implements RequestHandler
         }
 
         return ($this->closure)($formRequest)->send();
-    }
-
-    private function resolveFormRequest(ServerRequest $request): Request|FormRequest
-    {
-        $reflector = new ReflectionFunction($this->closure);
-
-        /** @var ReflectionParameter|null $parameter */
-        $parameter = $reflector->getParameters()[0] ?? null;
-
-        if (! $parameter) {
-            return new Request($request);
-        }
-
-        /** @var ReflectionNamedType|null $type */
-        $type = $parameter->getType();
-        $className = $type?->getName();
-
-        if ($className && is_subclass_of($className, FormRequest::class)) {
-            return new $className($request);
-        }
-
-        return new Request($request);
     }
 }
