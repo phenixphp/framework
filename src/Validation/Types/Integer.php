2a55de24641ae53b629d2c0f365e0a088c9254e9
Omar Barbosa <omar.barbosa@evertecinc.com>
2024-09-14 21:55:09 -0500

    refactor: reduce code duplication

--- src/Validation/Types/Integer.php
+++ src/Validation/Types/Integer.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace Phenix\Validation\Types;
 
 use Phenix\Validation\Rules\Between;
-use Phenix\Validation\Rules\In;
 use Phenix\Validation\Rules\Max;
 use Phenix\Validation\Rules\Min;
-use Phenix\Validation\Rules\NotIn;
-use Phenix\Validation\Rules\Numbers\Digits;
-use Phenix\Validation\Rules\Numbers\DigitsBetween;
 use Phenix\Validation\Rules\Numbers\IsInteger;
 use Phenix\Validation\Rules\TypeRule;
 
-class Integer extends Scalar
+class Integer extends Numeric
 {
     protected function defineType(): TypeRule
     {
@@ -38,34 +34,6 @@ class Integer extends Scalar
     public function between(int $min, int $max): self
     {
         $this->rules['between'] = Between::new($min, $max);
-
-        return $this;
-    }
-
-    public function digits(int $value): self
-    {
-        $this->rules['digits'] = Digits::new($value);
-
-        return $this;
-    }
-
-    public function digitsBetween(int $min, int $max): self
-    {
-        $this->rules['digits_between'] = DigitsBetween::new($min, $max);
-
-        return $this;
-    }
-
-    public function in(array $values): self
-    {
-        $this->rules['in'] = In::new(array_values($values));
-
-        return $this;
-    }
-
-    public function notIn(array $values): self
-    {
-        $this->rules['not_in'] = NotIn::new(array_values($values));
 
         return $this;
     }
