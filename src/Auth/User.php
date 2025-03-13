efd72395ee0d30dfc45c3a06204bad36996c8461
Omar Barbosa <omar.barbosa@evertecinc.com>
2025-03-13 09:09:58 -0500

    style: end empty line at end

---
+++ src/Auth/User.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phenix\Auth;
+
+use Phenix\Database\Models\DatabaseModel;
+
+abstract class User extends DatabaseModel
+{
+    public function createToken($name, array $habilities = []): string
+    {
+        return $this->tokens()->create([
+            'name' => $name,
+            'token' => hash('sha256', $token = random_bytes(40)),
+            'habilities' => $habilities,
+        ])->token;
+    }
+}
