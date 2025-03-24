bde6c4c5a0b12129fb1371c17983e8fb195c9a82
Omar Barbosa <omar.barbosa@evertecinc.com>
2025-03-23 21:47:51 -0500

    feat: view facade and service provider setup

---
+++ src/Views/Contracts/TemplateEngine.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phenix\Views\Contracts;
+
+use Closure;
+
+interface TemplateEngine
+{
+    public function view(string $template, array $data = []): View;
+
+    public function directive(string $name, Closure $closure): void;
+
+    public function clearCache(): void;
+}
