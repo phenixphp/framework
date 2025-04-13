d4a86489f434577cd87fb6930964d3f68b506199
Omar Barbosa <omar.barbosa@evertecinc.com>
2025-04-12 23:00:04 -0500

    refactor: update mail facade

--- src/Facades/Mail.php
+++ src/Facades/Mail.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 namespace Phenix\Facades;
 
-use Phenix\Mail\Contracts\Mailer;
 use Phenix\Mail\MailManager;
 use Phenix\Runtime\Facade;
 
 /**
- * @method static Mailer mailer(string|null $mailer = null)
- * @method static Mailer using(string $mailer)
- * @method static Mailer to(array|string $to)
- * @method static void send(Mailable $mailable, array $data = [], \Closure|null $callback = null)
- * @method static Mailer log(string|null $mailer = null)
+ * @method static \Phenix\Mail\Contracts\Mailer mailer(string|null $mailer = null)
+ * @method static \Phenix\Mail\Contracts\Mailer using(string $mailer)
+ * @method static \Phenix\Mail\Contracts\Mailer to(array|string $to)
+ * @method static void send(\Phenix\Mail\Contracts\Mailable $mailable, array $data = [], \Closure|null $callback = null)
+ * @method static \Phenix\Mail\Contracts\Mailer log(\Phenix\Mail\Constants\MailerDriver|null $mailer = null)
  *
  * @see \Phenix\Mail\MailManager
  */
