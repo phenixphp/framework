59cc37cb3f43b1c5c191ee2d75292d55f49db42b
Omar Barbosa <omar.barbosa@evertecinc.com>
2025-07-23 11:32:03 -0500

    refactor: update WorkerOptions constructor to use default values for properties

--- src/Queue/WorkerOptions.php
+++ src/Queue/WorkerOptions.php
@@ -7,6 +7,27 @@ namespace Phenix\Queue;
 class WorkerOptions
 {
     public function __construct(
-        public readonly int $sleep
+        public readonly int $sleep = 3,
+        public readonly int $maxTries = 3,
+        public readonly int $timeout = 60,
+        public readonly bool $once = false,
+        public readonly int $retryDelay = 5,
+        public readonly int $maxRetryDelay = 300,
+        public readonly int $batchSize = 1,
+        public readonly bool $processInBatch = false
     ) {}
+
+    public function toArray(): array
+    {
+        return [
+            'sleep' => $this->sleep,
+            'maxTries' => $this->maxTries,
+            'timeout' => $this->timeout,
+            'once' => $this->once,
+            'retryDelay' => $this->retryDelay,
+            'maxRetryDelay' => $this->maxRetryDelay,
+            'batchSize' => $this->batchSize,
+            'processInBatch' => $this->processInBatch,
+        ];
+    }
 }
