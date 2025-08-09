78fcb4f51353fb9e67d79cde5bc8ee9eb036e3c6
barbosa89 <contacto@omarbarbosa.com>
2025-08-09 14:26:16 -0500

    test: add unit test for handling failed tasks with database driver

--- tests/Unit/Queue/WorkerTest.php
+++ tests/Unit/Queue/WorkerTest.php
@@ -2,11 +2,16 @@
 
 declare(strict_types=1);
 
+use Amp\Sql\SqlTransaction;
+use Phenix\Database\Constants\Connection;
 use Phenix\Facades\Config;
 use Phenix\Queue\Constants\QueueDriver;
 use Phenix\Queue\QueueManager;
 use Phenix\Queue\Worker;
 use Phenix\Queue\WorkerOptions;
+use Tests\Mocks\Database\MysqlConnectionPool;
+use Tests\Mocks\Database\Result;
+use Tests\Mocks\Database\Statement;
 use Tests\Unit\Queue\Tasks\BadTask;
 use Tests\Unit\Queue\Tasks\SampleQueuableTask;
 
@@ -57,4 +62,55 @@ it('processes a failed task and retries', function (): void {
     $worker = new Worker($queueManager);
 
     $worker->daemon('default', 'custom-queue', new WorkerOptions(once: true, sleep: 1));
+});
+
+it('processes a failed task and retries using database driver', function (): void {
+    Config::set('queue.default', QueueDriver::DATABASE->value);
+
+    $connection = $this->getMockBuilder(MysqlConnectionPool::class)->getMock();
+    $transaction = $this->getMockBuilder(SqlTransaction::class)->getMock();
+
+    $databaseStatement = $this->getMockBuilder(Statement::class)
+        ->disableOriginalConstructor()
+        ->getMock();
+
+    $queuableTask = new BadTask();
+
+    $databaseStatement->expects($this->once())
+        ->method('execute')
+        ->willReturn(new Result([
+            [
+                'id' => $queuableTask->getTaskId(),
+                'payload' => $queuableTask->getPayload(),
+                'attempts' => 0,
+                'reserved_at' => null,
+                'available_at' => (new DateTime())->format('Y-m-d H:i:s'),
+                'created_at' => (new DateTime())->format('Y-m-d H:i:s'),
+            ],
+        ]));
+
+    $transaction->expects($this->exactly(2))
+        ->method('prepare')
+        ->willReturnOnConsecutiveCalls(
+            $databaseStatement,
+            new Statement(new Result([['Query OK']])),
+        );
+
+    $connection->expects($this->once())
+        ->method('beginTransaction')
+        ->willReturn($transaction);
+
+    $connection->expects($this->exactly(2))
+        ->method('prepare')
+        ->willReturn(
+            new Statement(new Result([['Query OK']])),
+        );
+
+    $queueManager = new QueueManager();
+
+    $this->app->swap(Connection::default(), $connection);
+
+    $worker = new Worker($queueManager);
+
+    $worker->runNextTask('default', 'default', new WorkerOptions(once: true, sleep: 1));
 });
