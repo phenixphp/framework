4f1653ee74922150dce885f89552ea980293cdb7
barbosa89 <contacto@omarbarbosa.com>
2025-08-09 18:55:56 -0500

    test: add unit test for processing successful tasks using database driver

--- tests/Unit/Queue/WorkerTest.php
+++ tests/Unit/Queue/WorkerTest.php
@@ -64,6 +64,57 @@ it('processes a failed task and retries', function (): void {
     $worker->daemon('default', 'custom-queue', new WorkerOptions(once: true, sleep: 1));
 });
 
+it('processes a successful task using database driver', function (): void {
+    Config::set('queue.default', QueueDriver::DATABASE->value);
+
+    $connection = $this->getMockBuilder(MysqlConnectionPool::class)->getMock();
+    $transaction = $this->getMockBuilder(SqlTransaction::class)->getMock();
+
+    $databaseStatement = $this->getMockBuilder(Statement::class)
+        ->disableOriginalConstructor()
+        ->getMock();
+
+    $queuableTask = new SampleQueuableTask();
+
+    $databaseStatement->expects($this->once())
+        ->method('execute')
+        ->willReturn(new Result([
+            [
+                'id' => $queuableTask->getTaskId(),
+                'payload' => $queuableTask->getPayload(),
+                'attempts' => 0,
+                'reserved_at' => null,
+                'available_at' => (new DateTime())->format('Y-m-d H:i:s'),
+                'created_at' => (new DateTime())->format('Y-m-d H:i:s'),
+            ],
+        ]));
+
+    $transaction->expects($this->exactly(2))
+        ->method('prepare')
+        ->willReturnOnConsecutiveCalls(
+            $databaseStatement,
+            new Statement(new Result([['Query OK']])),
+        );
+
+    $connection->expects($this->once())
+        ->method('beginTransaction')
+        ->willReturn($transaction);
+
+    $connection->expects($this->once())
+        ->method('prepare')
+        ->willReturn(
+            new Statement(new Result([['Query OK']])),
+        );
+
+    $queueManager = new QueueManager();
+
+    $this->app->swap(Connection::default(), $connection);
+
+    $worker = new Worker($queueManager);
+
+    $worker->runNextTask('default', 'default', new WorkerOptions(once: true, sleep: 1));
+});
+
 it('processes a failed task and retries using database driver', function (): void {
     Config::set('queue.default', QueueDriver::DATABASE->value);
 
