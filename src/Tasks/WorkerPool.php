649f37661643fb9282c921b5e3bc9ac48964f2ea
barbosa89 <contacto@omarbarbosa.com>
2025-08-10 13:58:10 -0500

    feat: add timeout handling to task submission in WorkerPool

--- src/Tasks/WorkerPool.php
+++ src/Tasks/WorkerPool.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Phenix\Tasks;
 
 use Amp\Parallel\Worker;
+use Amp\TimeoutCancellation;
 use Phenix\Tasks\Contracts\Task;
 
 class WorkerPool extends AbstractWorker
 {
     protected function submitTask(Task $parallelTask): Worker\Execution
     {
-        return Worker\submit($parallelTask);
+        $timeout = new TimeoutCancellation($parallelTask->getTimeout());
+
+        return Worker\submit($parallelTask, $timeout);
     }
 }
