86e3c512e62bc5f66d6078243c3e68aaa1b9bbdf
barbosa89 <contacto@omarbarbosa.com>
2025-08-13 20:46:41 -0500

    refactor: add popChunk method and update driver method in Queue facade

--- src/Facades/Queue.php
+++ src/Facades/Queue.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Phenix\Facades;
 
+use Phenix\Queue\Constants\QueueDriver;
+use Phenix\Queue\Contracts\Queue as QueueContract;
 use Phenix\Queue\QueueManager;
 use Phenix\Runtime\Facade;
 use Phenix\Tasks\QueuableTask;
@@ -12,10 +14,12 @@ use Phenix\Tasks\QueuableTask;
  * @method static void push(QueuableTask $task)
  * @method static void pushOn(string $queue, QueuableTask $task)
  * @method static QueuableTask|null pop()
+ * @method static array<int, QueuableTask> popChunk(int $limit = 10, string|null $queueName = null)
  * @method static int size()
  * @method static void clear()
  * @method static string getConnectionName()
  * @method static void setConnectionName(string $name)
+ * @method static QueueContract driver(QueueDriver|null $driverName = null)
  *
  * @see \Phenix\Queue\QueueManager
  */
