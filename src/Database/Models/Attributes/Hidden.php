41b3739802b258d9254eec2ebf420690c7351ac7
barbosa89 <contacto@omarbarbosa.com>
2025-09-27 17:45:30 -0500

    feat: implement Hidden attribute for database models

---
+++ src/Database/Models/Attributes/Hidden.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phenix\Database\Models\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+readonly class Hidden extends Column
+{
+    //
+}
