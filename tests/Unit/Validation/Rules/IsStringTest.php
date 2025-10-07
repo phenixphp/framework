706a8552388601be41d5403c07ad37a26ca2cb6b
barbosa89 <contacto@omarbarbosa.com>
2025-10-07 10:30:17 -0500

    tests: display field name for human when field is registered in fields of validation file

--- tests/Unit/Validation/Rules/IsStringTest.php
+++ tests/Unit/Validation/Rules/IsStringTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Phenix\Validation\Rules\IsString;
 
-it('fails is_string when value not a string', function () {
+it('fails when value not a string', function () {
     $rule = new IsString();
     $rule->setField('name')->setData(['name' => 123]);
 
@@ -12,9 +12,17 @@ it('fails is_string when value not a string', function () {
     assertStringContainsString('must be a string', (string) $rule->message());
 });
 
-it('passes is_string when value is a string', function () {
+it('passes when value is a string', function () {
     $rule = new IsString();
     $rule->setField('name')->setData(['name' => 'John']);
 
     assertTrue($rule->passes());
+});
+
+it('display field name for humans', function () {
+    $rule = new IsString();
+    $rule->setField('last_name')->setData(['last_name' => 123]);
+
+    assertFalse($rule->passes());
+    assertStringContainsString('last name must be a string', (string) $rule->message());
 });
