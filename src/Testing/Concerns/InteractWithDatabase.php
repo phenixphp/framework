af3fa9641b2d1d815f689c604c49f31d668275ed
barbosa89 <contacto@omarbarbosa.com>
2025-10-08 11:38:22 -0500

    refactor(InteractWithDatabase): simplify record count retrieval and update method signatures

--- src/Testing/Concerns/InteractWithDatabase.php
+++ src/Testing/Concerns/InteractWithDatabase.php
@@ -14,7 +14,7 @@ trait InteractWithDatabase
      */
     public function assertDatabaseHas(string $table, array $data): void
     {
-        [$count, ] = $this->getRecordCount($table, $data);
+        $count = $this->getRecordCount($table, $data);
 
         Assert::assertGreaterThan(0, $count, 'Failed asserting that table has matching record.');
     }
@@ -24,7 +24,7 @@ trait InteractWithDatabase
      */
     public function assertDatabaseMissing(string $table, array $data): void
     {
-        [$count, ] = $this->getRecordCount($table, $data);
+        $count = $this->getRecordCount($table, $data);
 
         Assert::assertSame(0, $count, 'Failed asserting that table is missing the provided record.');
     }
@@ -34,25 +34,21 @@ trait InteractWithDatabase
      */
     public function assertDatabaseCount(string $table, int $expected, array $data = []): void
     {
-        [$count, ] = $this->getRecordCount($table, $data);
+        $count = $this->getRecordCount($table, $data);
 
         Assert::assertSame($expected, $count, 'Failed asserting the expected database record count.');
     }
 
     /**
      * @param  array<string, scalar|bool|int|string|null>  $data
-     * @return array{0:int,1:string}
      */
-    protected function getRecordCount(string $table, array $data): array
+    protected function getRecordCount(string $table, array $data): int
     {
         $query = DB::from($table);
-
-        $descriptionParts = [];
 
         foreach ($data as $column => $value) {
             if ($value === null) {
                 $query->whereNull($column);
-                $descriptionParts[] = sprintf('%s IS NULL', $column);
 
                 continue;
             }
@@ -62,17 +58,8 @@ trait InteractWithDatabase
             }
 
             $query->whereEqual($column, is_int($value) ? $value : (string) $value);
-            $descriptionParts[] = sprintf('%s = %s', $column, var_export($value, true));
         }
 
-        if ($descriptionParts === []) {
-            $count = $query->count();
-
-            return [$count, ''];
-        }
-
-        $count = $query->count();
-
-        return [$count, implode(', ', $descriptionParts)];
+        return $query->count();
     }
 }
