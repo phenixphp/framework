39d329b2c553df824b81e3eb98aa91dd0bc6fd3e
barbosa89 <contacto@omarbarbosa.com>
2025-11-04 14:41:26 -0500

    feat(Table): implement automatic saving of columns on destruction

--- src/Database/Migrations/Table.php
+++ src/Database/Migrations/Table.php
@@ -31,9 +31,45 @@ class Table extends PhinxTable
      */
     protected array $columns = [];
 
+    protected bool $executed = false;
+
+    public function __destruct()
+    {
+        if (!$this->executed) {
+            $this->save();
+        }
+    }
+
     public function getColumnBuilders(): array
     {
         return $this->columns;
+    }
+
+    public function create(): void
+    {
+        $this->addColumnFromBuilders();
+
+        parent::create();
+
+        $this->executed = true;
+    }
+
+    public function update(): void
+    {
+        $this->addColumnFromBuilders();
+
+        parent::update();
+
+        $this->executed = true;
+    }
+
+    public function save(): void
+    {
+        $this->addColumnFromBuilders();
+
+        parent::save();
+
+        $this->executed = true;
     }
 
     /**
@@ -48,5 +84,12 @@ class Table extends PhinxTable
         $this->columns[] = $column;
 
         return $column;
+    }
+
+    protected function addColumnFromBuilders(): void
+    {
+        foreach ($this->columns as $column) {
+            $this->addColumn($column->getName(), $column->getType(), $column->getOptions());
+        }
     }
 }
