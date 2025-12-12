25d2de537912ac1bcbaef5917d85ae6b609657aa
barbosa89 <contacto@omarbarbosa.com>
2025-12-12 17:39:13 -0500

    refactor: simplify signal trapping function call in run method

--- src/App.php
+++ src/App.php
@@ -26,6 +26,7 @@ use Phenix\Logging\LoggerFactory;
 use Phenix\Runtime\Log;
 use Phenix\Session\SessionMiddlewareFactory;
 
+use function Amp\trapSignal;
 use function count;
 use function is_array;
 
@@ -94,7 +95,7 @@ class App implements AppContract, Makeable
         $this->server->start($this->router, $this->errorHandler);
 
         if ($this->signalTrapping) {
-            $signal = \Amp\trapSignal([SIGHUP, SIGINT, SIGQUIT, SIGTERM]);
+            $signal = trapSignal([SIGHUP, SIGINT, SIGQUIT, SIGTERM]);
 
             $this->logger->info("Caught signal {$signal}, stopping server");
 
