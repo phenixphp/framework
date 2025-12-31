ac761846e4eb5e1e19679e89e1801f879b34eee0
barbosa89 <contacto@omarbarbosa.com>
2025-12-31 19:05:02 +0000

    feat: implement base Dialect class and extend for MySQL, PostgreSQL, and SQLite

--- src/Database/Dialects/MySQL/MysqlDialect.php
+++ src/Database/Dialects/MySQL/MysqlDialect.php
@@ -4,94 +4,26 @@ declare(strict_types=1);
 
 namespace Phenix\Database\Dialects\MySQL;
 
-use Phenix\Database\Constants\Action;
-use Phenix\Database\Contracts\Dialect;
+use Phenix\Database\Dialects\Dialect;
 use Phenix\Database\Dialects\MySQL\Compilers\Delete;
 use Phenix\Database\Dialects\MySQL\Compilers\Exists;
 use Phenix\Database\Dialects\MySQL\Compilers\Insert;
 use Phenix\Database\Dialects\MySQL\Compilers\Select;
 use Phenix\Database\Dialects\MySQL\Compilers\Update;
-use Phenix\Database\QueryAst;
 
-class MysqlDialect implements Dialect
+class MysqlDialect extends Dialect
 {
-    protected Select $selectCompiler;
-
-    protected Insert $insertCompiler;
-
-    protected Update $updateCompiler;
-
-    protected Delete $deleteCompiler;
-
-    protected Exists $existsCompiler;
-
     public function __construct()
+    {
+        $this->initializeCompilers();
+    }
+
+    protected function initializeCompilers(): void
     {
         $this->selectCompiler = new Select();
         $this->insertCompiler = new Insert();
         $this->updateCompiler = new Update();
         $this->deleteCompiler = new Delete();
         $this->existsCompiler = new Exists();
-    }
-
-    public function compile(QueryAst $ast): array
-    {
-        return match ($ast->action) {
-            Action::SELECT => $this->compileSelect($ast),
-            Action::INSERT => $this->compileInsert($ast),
-            Action::UPDATE => $this->compileUpdate($ast),
-            Action::DELETE => $this->compileDelete($ast),
-            Action::EXISTS => $this->compileExists($ast),
-        };
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileSelect(QueryAst $ast): array
-    {
-        $compiled = $this->selectCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileInsert(QueryAst $ast): array
-    {
-        $compiled = $this->insertCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileUpdate(QueryAst $ast): array
-    {
-        $compiled = $this->updateCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileDelete(QueryAst $ast): array
-    {
-        $compiled = $this->deleteCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileExists(QueryAst $ast): array
-    {
-        $compiled = $this->existsCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
     }
 }
--- src/Database/Dialects/PostgreSQL/PostgresDialect.php
+++ src/Database/Dialects/PostgreSQL/PostgresDialect.php
@@ -4,90 +4,26 @@ declare(strict_types=1);
 
 namespace Phenix\Database\Dialects\PostgreSQL;
 
-use Phenix\Database\Constants\Action;
-use Phenix\Database\Contracts\Dialect;
+use Phenix\Database\Dialects\Dialect;
 use Phenix\Database\Dialects\PostgreSQL\Compilers\Delete;
 use Phenix\Database\Dialects\PostgreSQL\Compilers\Exists;
 use Phenix\Database\Dialects\PostgreSQL\Compilers\Insert;
 use Phenix\Database\Dialects\PostgreSQL\Compilers\Select;
 use Phenix\Database\Dialects\PostgreSQL\Compilers\Update;
-use Phenix\Database\QueryAst;
 
-class PostgresDialect implements Dialect
+class PostgresDialect extends Dialect
 {
-    private Select $selectCompiler;
-    private Insert $insertCompiler;
-    private Update $updateCompiler;
-    private Delete $deleteCompiler;
-    private Exists $existsCompiler;
-
     public function __construct()
+    {
+        $this->initializeCompilers();
+    }
+
+    protected function initializeCompilers(): void
     {
         $this->selectCompiler = new Select();
         $this->insertCompiler = new Insert();
         $this->updateCompiler = new Update();
         $this->deleteCompiler = new Delete();
         $this->existsCompiler = new Exists();
-    }
-
-    public function compile(QueryAst $ast): array
-    {
-        return match ($ast->action) {
-            Action::SELECT => $this->compileSelect($ast),
-            Action::INSERT => $this->compileInsert($ast),
-            Action::UPDATE => $this->compileUpdate($ast),
-            Action::DELETE => $this->compileDelete($ast),
-            Action::EXISTS => $this->compileExists($ast),
-        };
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileSelect(QueryAst $ast): array
-    {
-        $compiled = $this->selectCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileInsert(QueryAst $ast): array
-    {
-        $compiled = $this->insertCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileUpdate(QueryAst $ast): array
-    {
-        $compiled = $this->updateCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileDelete(QueryAst $ast): array
-    {
-        $compiled = $this->deleteCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
-    }
-
-    /**
-     * @return array{0: string, 1: array<int, mixed>}
-     */
-    private function compileExists(QueryAst $ast): array
-    {
-        $compiled = $this->existsCompiler->compile($ast);
-
-        return [$compiled->sql, $compiled->params];
     }
 }
