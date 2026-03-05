00ad5325f54aa50b0e232dbd2e535e63e95cb703
barbosa89 <contacto@omarbarbosa.com>
2026-03-05 21:20:15 +0000

    feat: url generator, signature validation

---
+++ src/Routing/Exceptions/RouteNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phenix\Routing\Exceptions;
+
+use InvalidArgumentException;
+
+class RouteNotFoundException extends InvalidArgumentException
+{
+    // ..
+}
