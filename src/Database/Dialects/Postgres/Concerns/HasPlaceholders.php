f0b4ca3be0a5519be8cb026598a816d4f504f46a
barbosa89 <contacto@omarbarbosa.com>
2026-05-04 08:52:26 -0500

    refactor: remove TODO comment for efficiency in HasPlaceholders trait

--- src/Database/Dialects/Postgres/Concerns/HasPlaceholders.php
+++ src/Database/Dialects/Postgres/Concerns/HasPlaceholders.php
@@ -6,7 +6,6 @@ namespace Phenix\Database\Dialects\Postgres\Concerns;
 
 trait HasPlaceholders
 {
-    // TODO: Refactor this to be more efficient and handle edge cases
     protected function convertPlaceholders(string $sql, int $startIndex = 0): string
     {
         $index = $startIndex + 1;
